3d2e77775d1396e6cef41b98455aa281bafdafe5
IbroJim <salme.i.i115@gmail.com>
2019-04-18 21:36:48 +0300

    model restoran

--- src/com/company/temel/Order/Order.java
+++ src/com/company/temel/Order/Order.java
@@ -1,4 +1,4 @@
-package com.company.temel;
+package com.company.temel.Order;
 
 import java.util.ArrayList;
 import java.util.Date;
--- src/com/company/temel/User/Barmen.java
+++ src/com/company/temel/User/Barmen.java
@@ -1,4 +1,4 @@
-package com.company.temel;
+package com.company.temel.User;
 
 public class Barmen {
 
